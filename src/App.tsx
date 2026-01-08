8872e770ed2c2f553d300e3634641f62e27a6a34
imjawad <imjawads@gmail.com>
2026-01-08 11:48:37 +0500

    feat: add scroll-to-top behavior on route changes
    - Created useScrollToTop hook to automatically scroll page to top when navigating between routes
    - Wrapped AppContent with the hook to ensure consistent behavior across all pages
    - Improves user experience by starting each page view at the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { EnquiryBasketProvider } from "./contexts/EnquiryBasketContext";
+import { useScrollToTop } from "./hooks/useScrollToTop";
 import { Toaster } from "./components/ui/sonner";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -18,34 +19,44 @@ import InfrastructureSystems from "./pages/InfrastructureSystems";
 import OperationsSupport from "./pages/OperationsSupport";
 import SolutionDetail from "./pages/SolutionDetail";
 
+function AppContent() {
+  useScrollToTop();
+  
+  return (
+    <>
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/enquiry" element={<Enquiry />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/style-guide" element={<StyleGuide />} />
+          
+          {/* Solutions Routes */}
+          <Route path="/solutions" element={<Solutions />} />
+          <Route path="/solutions/complete-facilities" element={<CompleteFacilities />} />
+          <Route path="/solutions/infrastructure-systems" element={<InfrastructureSystems />} />
+          <Route path="/solutions/operations-support" element={<OperationsSupport />} />
+          <Route path="/solutions/:tier/:slug" element={<SolutionDetail />} />
+        </Routes>
+      </main>
+      <Footer />
+      <WhatsAppButton />
+      <Toaster position="top-right" />
+    </>
+  );
+}
+
 export default function App() {
   return (
     <HashRouter>
       <EnquiryBasketProvider>
         <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:id" element={<ProductDetail />} />
-              <Route path="/enquiry" element={<Enquiry />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/resources" element={<Resources />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/style-guide" element={<StyleGuide />} />
-              
-              {/* Solutions Routes */}
-              <Route path="/solutions" element={<Solutions />} />
-              <Route path="/solutions/complete-facilities" element={<CompleteFacilities />} />
-              <Route path="/solutions/infrastructure-systems" element={<InfrastructureSystems />} />
-              <Route path="/solutions/operations-support" element={<OperationsSupport />} />
-              <Route path="/solutions/:tier/:slug" element={<SolutionDetail />} />
-            </Routes>
-          </main>
-          <Footer />
-          <WhatsAppButton />
-          <Toaster position="top-right" />
+          <AppContent />
         </div>
       </EnquiryBasketProvider>
     </HashRouter>
